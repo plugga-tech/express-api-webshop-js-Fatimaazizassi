673690bf3a3bac4222c378a1bd987aba54eafe6f
Fatima Assi <Fatima.assi@medieinstitutet.se>
2024-02-11 11:02:49 +0100

    Edited orders router

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongodb = require('mongodb')
 
 /* GET all orders */
 router.get('/all', function (req, res, next) {
@@ -11,10 +12,24 @@ router.get('/all', function (req, res, next) {
 
 /* POST/Add new order */
 router.post('/add', function(req, res, next){
-  req.app.locals.db.collection('orders').insertOne(req.body)
-  .then(results => {
-    res.json(results)
-  })
+  const products = req.body.products
+
+  if (products) {
+    for (p in products) {
+      /* Edit quantity of the prroduct lager */
+      editProductLagerQuantity(req, products[p].productId, products[p].quantity)
+    }
+
+    req.app.locals.db.collection('orders').insertOne(req.body)
+    .then(results => {
+      res.json(results)
+    })
+  }
 })
+
+function editProductLagerQuantity(req, pId, pCu) {
+  const id = new mongodb.ObjectId(pId)
+  req.app.locals.db.collection('products').updateOne({_id: id}, { $inc: { lager: -pCu }})
+}
 
 module.exports = router;
